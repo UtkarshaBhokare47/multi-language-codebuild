8a0c8539d24f9ab6faa89856e1bdb1477da7144a
Utkarsha U. Bhokare <utkarshabhokare47@gmail.com>
2026-06-15 17:13:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-numbers = [10, 20, 30, 40, 50]
+numbers = [10, 25, 8, 45, 17, 30]
 
 highest = max(numbers)
 lowest = min(numbers)
 average = sum(numbers) / len(numbers)
 
+print("Numbers:", numbers)
 print("Highest:", highest)
 print("Lowest:", lowest)
 print("Average:", average)
